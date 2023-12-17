8a9b0702ef68144dcf47d566fe5f790e97004813
YeabNigusse <nigusseyeabsira@gmail.com>
2023-12-18 02:51:48 +0300

    sth

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Collection(models.Model):
+    type = models.CharField(max_length=255)
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     discription = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete = models.PROTECT)
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=255)
@@ -26,4 +30,15 @@ class Order(models.Model):
     ]
     palced_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=status_choice, default=Pending_status)
-    
+    customer = models.ForeignKey(Customer, on_delete = models.PROTECT)
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.OneToOneField(Customer, on_delete = models.CASCADE, primary_key = True)
+
+# the parent class goes to chiled class to tell the relationship
+
+# the primary key help us to create onto one relationship between addres and customer
+# the above code shows the way we create tables in database using python code only without database
+# choice field shows options we insert in the database
